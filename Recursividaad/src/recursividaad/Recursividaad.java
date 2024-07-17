de8cecebe6c656ec70b5269fdf150504fc14c6d7
Gbriel2003 <gcardona.9024@unimar.edu.ve>
2024-07-16 22:27:32 -0400

    agrego el proyecto con el primer ejercicio listo

---
+++ Recursividaad/src/recursividaad/Recursividaad.java
@@ -0,0 +1,56 @@
+package recursividaad;
+
+import java.util.Scanner;
+
+
+public class Recursividaad {
+
+    public static void main(String[] args) {
+        // EJERCICIO 1 
+       Scanner entrada = new Scanner(System.in);
+        
+        int size = 0;
+        while(true){ // VALIDACION POR SI EL USUARIO INGRESA UN TAMAÑO INVALIDO DEL ARREGLO 
+            System.out.print("Ingrese el tamaño del arreglo: ");
+            if (entrada.hasNextInt()){
+                size = entrada.nextInt();
+                if (size > 0) {
+                    break;
+                } else {
+                    System.out.println("Por favor ingrese un numero positivo"); 
+                } 
+            } 
+        }
+        
+        int[] numeros = new int[size];
+        
+        for (int i= 0; i<size; i++){
+            while(true){ // VALIDACION POR SI EL USUARIO INGRESA UNA LETRA O UN CARACTER NO VALIDO PARA LA SUMA 
+                System.out.print("\n" + i +"._ ingrese los valores del arreglo: ");
+                if (entrada.hasNextInt()){
+                    numeros[i] = entrada.nextInt();
+                    break;
+                } else{
+                    System.out.println("Ingrese un dato valido (un numero entero)");
+                    entrada.next();
+                }
+            }
+        }
+        int TotalSum = SumaArreglo(numeros, size - 1);
+        System.out.println("La suma total del arreglo es: "+ TotalSum);
+        entrada.close();
+       
+    }
+         public static int SumaArreglo(int[] arreglo, int indice) {
+        // Caso base: si el índice es menor que 0, significa que hemos terminado de sumar
+        if (indice < 0) {
+            return 0;
+        } else {
+            // Caso recursivo: sumamos el elemento actual al resultado de sumar el resto
+            return arreglo[indice] + SumaArreglo(arreglo, indice - 1);
+        }
+    }   
+      
+     
+      
+}
